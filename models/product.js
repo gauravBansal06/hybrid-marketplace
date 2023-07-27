46db8325d7601cf6508ef468e5149022623a1155
Gaurav Bansal <gauravb834@gmail.com>
2023-07-27 22:37:31 +0530

    products db schema

---
+++ models/product.js
@@ -0,0 +1,50 @@
+const { sequelize, DataTypes } = require('../config/database')
+const { Catalog } = require('./catalog')
+
+const Product = sequelize.define(
+    'product',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            length: 50,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+        },
+        catalogId: {
+            type: DataTypes.UUID
+        },
+        iaActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
+    },
+    {
+        freezeTableName: true,
+        underscored: true
+    }
+)
+
+Catalog.hasMany(Product, {
+    foreignKey: {
+        name: 'catalog_id',
+        allowNull: false
+    } 
+})
+Product.belongsTo(Catalog)
+/*
+Assuming one product belongs to one catalog only and different sellers have different products.
+This can easily be changed into m:m relationship if same product belongs to multiple sellers
+*/
+
+module.exports = {
+    Product
+}
