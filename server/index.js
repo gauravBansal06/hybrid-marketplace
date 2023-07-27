acaeebd4ef28b130dba6769fa5f51f83f715fe33
Gaurav Bansal <gauravb834@gmail.com>
2023-07-27 23:53:07 +0530

    added global middleware and routes registration

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const { Server } = require('../config')
+const router = require('../api')
+const { LoggerMiddleware } = require('../middleware')
 
 let httpServer
 const app = express()
 
 app.use(express.json())
+app.use(LoggerMiddleware)
+app.use(`/api/${Server.Version}`, router) //making every route - "http://localhost:port/api/v1/{route}"
 
 function StartServer() {
     try {
         httpServer = app.listen(Server.Port, () => {
             const { port } = httpServer.address()
-            console.log(`🤘 Successfully Started Server: http://localhost:${port}`)
+            console.log(`Successfully Started Server: http://localhost:${port}`)
         })
     } catch (error) {
         console.log(`Error in starting server - ${error}`)
