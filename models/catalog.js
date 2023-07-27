43103e2b40100ed3fc67ee9e7cabc967b6891f42
Gaurav Bansal <gauravb834@gmail.com>
2023-07-27 22:37:42 +0530

    catalog schema

---
+++ models/catalog.js
@@ -0,0 +1,49 @@
+const { Seller } = require('./seller')
+const { sequelize, DataTypes } = require('../config/database')
+
+const Catalog = sequelize.define(
+    'catalog',
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            length: 50,
+            allowNull: false
+        },
+        sellerId: {
+            type: DataTypes.UUID
+        },
+        iaActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
+    },
+    {
+        freezeTableName: true,
+        underscored: true,
+        indexes: [
+            {
+                unique: true,     //overrides index by foreign key and create unique index
+                fields: ['seller_id']
+            }
+        ],
+        
+    }
+)
+
+Catalog.belongsTo(Seller, {
+    foreignKey: {
+        name: 'seller_id',
+        allowNull: false
+    }
+})
+Seller.hasOne(Catalog)
+
+module.exports = {
+    Catalog
+}
