1ca57cfc75b1e8157f5e1b08a78a2f419feb4930
Gaurav Bansal <gauravb834@gmail.com>
2023-07-27 22:39:11 +0530

    mysql db connection configs

---
+++ config/database.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes}  = require('sequelize')
+const { MySqlDB } = require('.')
+
+const sequelize = new Sequelize(
+    MySqlDB.Name, MySqlDB.User, MySqlDB.Password,
+    {
+        host: MySqlDB.Host,
+        dialect: 'mysql',
+    }
+)
+
+function InitDatabase(){
+    return new Promise((resolve, reject) => {
+        sequelize.authenticate().then(() => {
+            console.log('Successfully Connected to MySQL DB');
+            resolve('success')
+        }).catch((error) => {
+            console.log('Error connecting to MySQL DB - ', JSON.stringify(error))
+            reject('failed')
+        })
+    })
+}
+
+function StopDatabase(){
+    return new Promise((resolve, reject) => {
+        sequelize.close().then(() => {
+            console.log('Successfully closed connection to MySQL DB');
+            resolve('success')
+        }).catch((error) => {
+            console.log('Error closing connection to MySQL DB - ', JSON.stringify(error))
+            reject('failed')
+        })
+    })
+}
+
+module.exports = {
+    sequelize,
+    DataTypes,
+    InitDatabase,
+    StopDatabase
+}
