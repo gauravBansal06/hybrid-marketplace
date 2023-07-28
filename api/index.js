b4541f96737815cdf74097cfbff68fd7b51d07e4
Gaurav Bansal <gauravb834@gmail.com>
2023-07-28 22:49:43 +0530

    added auth middleware check

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const { Router } = require('express')
+const { AuthMiddleware } = require('../middleware')
 const healthCheckRouter = require('./healthCheck')
 const authRouter = require('./auth')
 
 const router = Router()
 
-router.use(healthCheckRouter)
+router.use('/health', healthCheckRouter)
 router.use('/auth', authRouter)
+
+router.use(AuthMiddleware) //after this, authentication required for each api
 
 module.exports = router
