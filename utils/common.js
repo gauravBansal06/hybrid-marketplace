7968699a36ddc89e89839775535bbecfb4c5fa84
Gaurav Bansal <gauravb834@gmail.com>
2023-07-28 19:21:27 +0530

    added encryption helpers

--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,15 @@
+const bcrypt = require('bcryptjs')
+
+const EncryptPassword = (password) => {
+    const encryptedPwd = bcrypt.hashSync(password)
+    return encryptedPwd
+}
+
+const ComparePassword = async (password, encryptedPassword) => {
+    const isSame = await bcrypt.compareSync(password, encryptedPassword)
+    return isSame
+}
+
 const FormatApiResponse = (statusCode, data = null, message = null , error = null) => {
     return {
         statusCode,
@@ -7,6 +19,14 @@ const FormatApiResponse = (statusCode, data = null, message = null , error = nul
     }
 }
 
+const ValidateAlphaNumericString = (testString) => {
+    const alphaRegExp = new RegExp(/^[a-z0-9]+$/i)
+    return alphaRegExp.test(testString) 
+}
+
 module.exports  = {
-    FormatApiResponse
+    EncryptPassword,
+    ComparePassword,
+    FormatApiResponse,
+    ValidateAlphaNumericString
 }
