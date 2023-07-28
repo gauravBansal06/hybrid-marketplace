fe1e0b4679d9f9b3e02a991efecb564da429336f
Gaurav Bansal <gauravb834@gmail.com>
2023-07-28 22:48:36 +0530

    authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,22 @@
+const { AuthorizationHeaderKey, StatusCodes } = require('../constants/http')
+const { VerifyAuthJwtToken } = require('../utils/jwt')
+
+const AuthenticateUser = (req, res, next) => {
+    const token = req.headers[AuthorizationHeaderKey]
+    if (!token) {
+        return res.status(StatusCodes.UserUnauthorised).send(`${AuthorizationHeaderKey} header is missing`);
+    }
+    try {
+        const decodedPayload = VerifyAuthJwtToken(token)
+        req.user = {
+            id: decodedPayload.id,
+            userType: decodedPayload.userType,
+            sellerId: decodedPayload.sellerId
+        }
+    } catch (err) {
+        return res.status(StatusCodes.UserUnauthorised).send("Invalid Token! Please Register or Login");
+    }
+    return next();
+}
+
+module.exports = AuthenticateUser
