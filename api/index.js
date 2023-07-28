3ff8f28c02a5ddc7064016dc075546525d456666
Gaurav Bansal <gauravb834@gmail.com>
2023-07-28 20:29:47 +0530

    renamed user to auth apis

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
 const healthCheckRouter = require('./healthCheck')
-const userRouter = require('./user')
+const authRouter = require('./auth')
 
 const router = Router()
 
 router.use(healthCheckRouter)
-router.use('/user', userRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
