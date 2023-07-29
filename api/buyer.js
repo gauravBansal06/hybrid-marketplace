c38048267c7e34a05c33601be463d921a3d8b566
Gaurav Bansal <gauravb834@gmail.com>
2023-07-29 14:33:01 +0530

    added get-seller-catalog api

--- api/buyer.js
+++ api/buyer.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const { GetSellerFullCatalog } = require('../service/catalog')
 const { GetListofAllSellers } = require('../service/user')
 
 const router = Router()
@@ -6,6 +7,16 @@ const router = Router()
 router.get('/list-of-sellers', async (req, res, next) => {
     try {
         const result = await GetListofAllSellers()
+        res.status(result.statusCode).json(result)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/seller-catalog/:seller_id', async (req, res, next) => {
+    try {
+        const sellerId = req.params['seller_id']
+        const result = await GetSellerFullCatalog(sellerId)
         res.status(result.statusCode).json(result)
     } catch (error) {
         next(error)
